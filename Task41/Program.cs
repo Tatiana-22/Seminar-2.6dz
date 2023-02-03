020ebf642f3e81f47f23606c4a2bd8f8dc2c5897
Tanya <Tanya2204frosya@gmail.com>
2023-02-04 02:18:55 +0300

    first commit

---
+++ Task41/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+System.Console.WriteLine("Введите длину массива");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] myArr = GetArray(length);
+int result = CountNum(myArr);
+System.Console.WriteLine("Результат " + result);
+
+int[] GetArray(int len)
+{
+    int[] newArr = new int[len];
+    for(int i=0; i<len;i++)
+    {
+        System.Console.WriteLine($"Введите значение {i} элемента массива");
+        newArr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return newArr;
+}
+
+int CountNum(int[] arr)
+{
+    int res = 0;
+    for(int i=0; i<arr.Length;i++)
+    {
+        if (arr[i]>0)
+        {
+            res++;
+        }
+    }
+    return res;
+}
---
+++ Task43/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double b1 = new Random().Next(0,3);
+Console.WriteLine($"Переменная b1 равна {b1}");
+double b2 = new Random().Next(0,5);
+Console.WriteLine($"Переменная b2 равна {b2}");
+double k1 = new Random().Next(0,4);
+Console.WriteLine($"Переменная k1 равна {k1}");
+double k2 = new Random().Next(0,5);
+Console.WriteLine($"Переменная k2 равна {k2}");
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+ 
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+ 
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
+
+  
+  
+  
+  
+
+
+
+
